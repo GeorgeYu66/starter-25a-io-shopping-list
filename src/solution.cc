96a19b85fd637e6ecfca31359b9cc95488710e1d
George Yu <gyu13@illinois.edu>
2025-03-07 22:47:01 -0600

    solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -2,11 +2,28 @@
 
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <stdexcept>
 #include <string>
 
 std::vector<Item> LoadShoppingItemsFromFile(const std::string& filename) {
+  std::ifstream nmsl(filename);
+  if (!nmsl.is_open()) {
+    throw std::invalid_argument("NMSL");
+  }
   std::vector<Item> shopping_items;
-
+  Item cnm;
+  while (nmsl.good()) {
+    nmsl >> cnm.item_name >> cnm.quantity >> cnm.price;
+    if (nmsl.fail()) {
+      nmsl.clear();
+      nmsl.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } else {
+      shopping_items.push_back(cnm);
+    }
+  }
+  if (shopping_items.empty()) {
+    throw std::invalid_argument("NMSL");
+  }
   return shopping_items;
 }

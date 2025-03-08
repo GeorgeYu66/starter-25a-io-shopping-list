6c4b68ca1704da9d5333510dfc2f979a0c77abf5
George Yu <gyu13@illinois.edu>
2025-03-08 02:34:49 +0000

    NMSL

--- src/solution.cc
+++ src/solution.cc
@@ -7,11 +7,20 @@
 
 std::vector<Item> LoadShoppingItemsFromFile(const std::string& filename) {
   std::ifstream nmsl(filename);
+  if (!nmsl.is_open()) {
+    throw std::invalid_argument("NMSL");
+  }
   std::vector<Item> shopping_items;
   Item cnm;
   while (nmsl.good()) {
     std::cin >> cnm.item_name >> cnm.quantity >> cnm.price;
     shopping_items.push_back(cnm);
+    if (nmsl.fail()) {
+      throw std::invalid_argument("NMSL");
+    }
+  }
+  if (shopping_items.empty()) {
+    throw std::invalid_argument("NMSL");
   }
   return shopping_items;
 }
